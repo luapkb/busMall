2ad42a63fe9e8645a9a9104d095bb54d63e4a501
paul bennett <luapkb@gmail.com>
2019-09-25 14:36:06 -0700

    click vote

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 
-
 //!!!!!!is the Js file pluged into the html!!!!
 ////start     global variablils    start
+
+///array of names
+var assetArrayName = ['bag','banana','bathroom', 'boots', 'breakfast', 'bubblegum', 'chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+
+// array of images
+var assetArrayPath = ['assets/bag.jpg','assets/banana.jpg','assets/bathroom.jpg', 'assets/boots.jpg', 'assets/breakfast.jpg', 'assets/bubblegum.jpg', 'assets/chair.jpg','assets/cthulhu.jpg','assets/dog-duck.jpg','assets/dragon.jpg','assets/pen.jpg','assets/pet-sweep.jpg','assets/scissors.jpg','assets/shark.jpg','assets/sweep.png','assets/tauntaun.jpg','assets/unicorn.jpg','assets/usb.gif','assets/water-can.jpg','assets/wine-glass.jpg'];
+
 //section tittle id
 var pictures = document.getElementById('pictures');
-//image sorce 123
+//image ID (sorce 123)
 var choice1 = document.getElementById('choice1');
 var choice2 = document.getElementById('choice2');
 var choice3 = document.getElementById('choice3');
@@ -13,14 +19,14 @@ CreatChoice.allImage = [];
 choice1.src = null;
 choice2.src = null;
 choice3.src = null;
-
 //datta extractor
 var peoplesChoice = 0;
 //
 ////end   global variables  end  //
 console.log('start works');
-//constructoer reconstruction
-//funtio name and properties (atributs 'itsname' , theimage)
+
+//function name and properties (atributs 'itsname' , theimage)
+
 function CreatChoice (name, image){
   //properties in object
   this.name = name;
@@ -29,15 +35,21 @@ function CreatChoice (name, image){
   this.view = 0;
 
   CreatChoice.allImage.push (this);
-}
+  
+}  
 
+function populateCreatChoiceAllImage(){
+  for (var i = 0 ; i < assetArrayName.length; i ++){
+    new CreatChoice (assetArrayName[i], assetArrayPath[i]);
+  }  
+}  
 
 //random source generator from array CreatChoice.allimages in constructor
 function choiceGeni(){
   var raySelect = Math.floor(Math.random() * CreatChoice.allImage.length );
   console.log('raySelectt', raySelect);
   return raySelect;
-}
+}  
 // ///end funtion raySelect is the random image.
 
 // // do somethiig with that photo
@@ -48,27 +60,64 @@ function renderChoice(){
     var choice2Index = choiceGeni();
     var choice3Index = choiceGeni();
 
-  // the 3 part nolove solution needs help would a swich be apopriate.
+  // the 3 part nolove solution needs help would a swich be apopriate.  
   }while (choice1Index === choice2Index || choice1Index === choice3Index || choice2Index === choice3Index);
 
 
   //add one tothe views of the object Property view=0 fo rthe data exctractor peoplsChoice.
 
-  // choice1.allImage[choice1].view++;
-  // choice2.allImage[choice2].view++;
-  // choice3.allImage[choice3].view++;
 
   //send it to the html to be a choice
+
+  //populat click count
   choice1.src = CreatChoice.allImage[choice1Index].image;
   choice1.alt = CreatChoice.allImage[choice1Index].name;
   choice2.src = CreatChoice.allImage[choice2Index].image;
   choice2.alt = CreatChoice.allImage[choice2Index].name;
   choice3.src = CreatChoice.allImage[choice3Index].image;
   choice3.alt = CreatChoice.allImage[choice3Index].name;
+  //populat view count
+  CreatChoice.allImage[choice1Index].view++;
+  CreatChoice.allImage[choice2Index].view++;
+  CreatChoice.allImage[choice3Index].view++;
 
-// console.log(event, target , id);
-}
-// //end funtion renderchoice  end
+}  
+///////////unique array from class(Ron)
+// //uniqueArray is where you will store the unique numbers that displayPics() creates in the while loops.
+// //productArray will be wherever you are pushing your new instances to, so make changes as needed.
+// //you will also need to change the variable name of makeRandomNumber to your own function's name.
+
+// var uniqueArray = [];
+
+// function displayPics() {
+//   //keeps the array filled with 6 unique values  
+//   while(uniqueArray.length < 6) {
+//     var random = makeRandomNumber();  
+//     while(!uniqueArray.includes(random)) {
+//       console.log('building uniqueArray:  ', uniqueArray);  
+//       uniqueArray.push(random);
+//     }
+//   }
+//   console.log('uniqueArray completed!!: ', uniqueArray);
+//   for( var i = 0; i < uniqueArray.length; i++ ) {
+//   //value of the first index of the array is removed and set as the variable 'temp' and replaced at each iteration of the loop.  
+//     var temp = uniqueArray.shift();
+//     console.log('The Temp is #: ', temp);
+//     //sets the path of the product at the current index.
+//     Product.pics[i].src = productArray[temp].path;
+//     //sets the id of the product at the current index.
+//     Product.pics[i].id = productArray[temp].name;
+//    productArray[temp].views += 1;
+//     console.log('Product.all[temp].name: ',productArray[temp].name);
+//   }
+//   console.log('remaining values in uniqueArray: ', uniqueArray);
+// }
+///////////unique array from class(Ron)
+  // console.log(event, target , id);
+
+
+
+// //end funtion renderchoice  end  
 
 // /// start   handle the user choice     start
 
@@ -79,37 +128,38 @@ function handleClickOnChoice(event){
   console.log(clickChoice);
   //when we clikkity
   for (var i = 0; i < CreatChoice.allImage.length; i++){
-  console.log(CreatChoice.allImage[i].image);
+    console.log(CreatChoice.allImage[i].image);
     if (clickChoice === CreatChoice.allImage[i].name){
-    CreatChoice.allImage[i].click++;
-    console.table(CreatChoice.allImage);
+      CreatChoice.allImage[i].click++;
+      console.table(CreatChoice.allImage);
+    }  
+  }  
   }
-   }
   // if (clickChoice === choice1 || clickChoice === choice2 || clickChoice === choice3 ){
 
-  //   peoplesChoice++;
+  //   peoplesChoice++;  
   //   // we dont know which one yet buthe the count goes up
   //   if (clickChoice === choice1){
-  //     CreatChoice.allImage[choice1Index].click++;
+  //     CreatChoice.allImage[choice1Index].click++;  
   //   }else if( clickChoice === choice2){
-  //     CreatChoice.allImage[choice2Index].click++;
+  //     CreatChoice.allImage[choice2Index].click++;  
   //   }else if (clickChoice === choice3Index){
-  //     CreatChoice.allImage[choice3Index].click++;
+  //     CreatChoice.allImage[choice3Index].click++;  
   //   } else{
-  //     alert ('you didn\'t select an Image.');
+  //     alert ('you didn\'t select an Image.');  
 
   //   }
   // }
-}
+
 
 // if (peoplesChoice === 5){
-// pictuers.removerEventListener ('click', handleClickOnChoice);
+// pictuers.removerEventListener ('click', handleClickOnChoice);  
 // alert = ('your finished with the choosing');
 // for (var i=0; i < CreatChoice.allImage.length; i++){
-//   var CreatChoice = CreatChoice.allImage[i];
+//   var CreatChoice = CreatChoice.allImage[i];  
 //   console.log('$ {CreatChoice.name} recived ${CreatChoice.click} vote with ${createChoice.view}');
 // }  else {
-//     renderChoice();
+//     renderChoice();  
 //   }
 // }
 
@@ -121,42 +171,11 @@ function handleClickOnChoice(event){
 
 /////////////////////////////////////////////////////////////// 23:20 9-23-19 demo refrece  line-86-87
 
-function populateCreatChoiceAllImage(){
-  for (var i = 0 ; i < assetArrayName.length; i ++){
-    new CreatChoice (assetArrayName[i], assetArrayPath[i]);
-  }
-}
-
-var assetArrayName = ['bag','banana','bathroom', 'boots', 'breakfast', 'bubblegum', 'chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
-
-
-// array of images
-
-var assetArrayPath = ['assets/bag.jpg','assets/banana.jpg','assets/bathroom.jpg', 'assets/boots.jpg', 'assets/breakfast.jpg', 'assets/bubblegum.jpg', 'assets/chair.jpg','assets/cthulhu.jpg','assets/dog-duck.jpg','assets/dragon.jpg','assets/pen.jpg','assets/pet-sweep.jpg','assets/scissors.jpg','assets/shark.jpg','assets/sweep.png','assets/tauntaun.jpg','assets/unicorn.jpg','assets/usb.gif','assets/water-can.jpg','assets/wine-glass.jpg'];
 
 
 
-// //   START     TAKE-ONE     START
-// function renderAsset(){
-// var choice1= document.getElementById(choice1);
-// var choice2= document.getElementById(choice2);
-
-// var randomAsset1= renderAsset();
-// var randomAsset2= renderAsset();
-
-// do{
-//   var randomAsset1= renderAsset();
-//   var randomAsset2= renderAsset();
-//   }while (randomAsset1 === randomAsset2);
-
-//   // set the source of the image tag
-
-// choice1.src=  imageObject.allImage[randomAsset1].Image;
-// choice2.src = imageObject.allImage[randomAsset2].image;
-
-// }
 
 populateCreatChoiceAllImage();
-console.log (CreatChoice.allImage);
+console.table (CreatChoice.allImage);
 renderChoice();
 pictures.addEventListener('click', handleClickOnChoice);
